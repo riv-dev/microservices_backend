9c1a3755be431892c9bcd790d5c4b9737f8637b2
Ken Le <ken@ryukyu-i.co.jp>
2017-06-07 13:16:48 +0700

    DB update for projects

---
+++ projects/models/db_patches/db_patch_060617.js
@@ -0,0 +1,75 @@
+var mysql = require('mysql')
+var credentials = require('../../credentials.js');
+
+var Patches = function() {
+
+}
+
+//Static Methods and Variables
+Patches.db = "Yo!";
+
+Patches.connect = function () {
+  this.db = mysql.createConnection({
+    host: credentials.mysql.host,
+    user: credentials.mysql.username,
+    password: credentials.mysql.password,
+  });
+
+  this.db.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+  });
+}
+
+Patches.disconnect = function () {
+  this.db.end()
+}
+
+Patches.apply_patch = function(call_back) {
+  Patches.connect();
+
+  console.log("apply_patch called.");
+
+  this.db.query('USE ryukyu_social_projects_service;', function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
+
+  this.db.query('ALTER TABLE projects CHANGE status status_code int DEFAULT 0;', function(err) {
+    if(err) {
+      console.log(err);
+    } 
+  });
+
+  this.db.query('ALTER TABLE project_users CHANGE status status_code int DEFAULT 0;', function(err) {
+    if(err) {
+      console.log(err);
+    } 
+  });  
+ 
+  Patches.disconnect();
+
+}
+
+Patches.reverse_patch = function(call_back) {
+  Patches.connect();
+
+  this.db.query('ALTER TABLE projects CHANGE status_code status varchar(255) DEFAULT "New";', function(err) {
+    if(err) {
+      console.log(err);
+    } 
+  });
+
+  this.db.query('ALTER TABLE project_users CHANGE status status_code int DEFAULT 0;', function(err) {
+    if(err) {
+      console.log(err);
+    } 
+  });
+
+  Patches.disconnect();
+}
+
+module.exports = Patches;
---
+++ projects/patch.js
@@ -0,0 +1,4 @@
+var patches = require("./models/db_patches/db_patch_060617.js");
+
+//Apply or unapply patches here
+patches.apply_patch();

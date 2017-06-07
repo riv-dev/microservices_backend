9758bd7347a41ce6de60586ce4b88e93d22349d4
Ken Le <ken@ryukyu-i.co.jp>
2017-06-07 13:09:58 +0700

    Edit tasks db

--- tasks/patch.js
+++ tasks/patch.js
@@ -1,4 +1,4 @@
-var patches = require("./models/db_patches/db_patch_060517.js");
+var patches = require("./models/db_patches/db_patch_060617.js");
 
 //Apply or unapply patches here
 patches.apply_patch();

a155424722783dd937dbb21908d679f55e40704c
Ken Le <ken@ryukyu-i.co.jp>
2017-06-28 11:54:05 +0700

    Added Slack Task Puller Service

---
+++ slack_task_puller/app.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'net/http'
+require 'net/https'
+require 'uri'
+require './slack_task_puller.rb'
+require './credentials.rb'
+
+uri = URI.parse("https://ryukyu-social.cleverword.com/tasks_service/api/tasks")
+https = Net::HTTP.new(uri.host,uri.port)
+https.use_ssl = true
+request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json',
+                                                      'x-access-token' => Credentials.tasks_service_token})
+
+
+#REST API Endpoints
+
+get '/' do
+  'This is the Slack Task Grabber Microservice!'
+end
+
+get '/new-slack-tasks' do
+  tasks = SlackTaskPuller.get_new_tasks
+
+  #Post to Ryukyu Social API with token and new task for each task
+  responses = []
+  tasks.each do |task|
+    request.set_form_data(task)
+    http_response = https.request(request)
+
+    response = {}
+    response["ryukyu_tasks_service_response"] = http_response.body
+    response["task"] = task
+
+    responses << response
+  end
+
+  responses.to_json
+end
---
+++ slack_task_puller/credentials.example.rb
@@ -0,0 +1,11 @@
+def Credentials
+
+    def self.slack_token
+        "abcd"
+    end
+
+    def self.tasks_service_token
+        "abcd"
+    end
+
+end

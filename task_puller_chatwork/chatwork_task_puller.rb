f8b4b31954a670e538a7ec14716421e7639089c6
Ken Le <ken@ryukyu-i.co.jp>
2017-06-29 15:59:52 +0700

    Add Chatwork task id

--- task_puller_chatwork/chatwork_task_puller.rb
+++ task_puller_chatwork/chatwork_task_puller.rb
@@ -23,8 +23,8 @@ class ChatworkTaskPuller
             if /^\s*@task\s+/.match(text)
               puts "Adding a new task!!!"
               tasks << {"name" => "Chatwork Task from #{user_name} in ##{room_name} at #{time.strftime('%c')}", "description" => text}
-            elsif results = /\[info\]\[title\]\[dtext:task_added\].*?\slt=(\d+)\](.*)?\[\/task\]/m.match(text)
-              tasks << {"name" => "Chatwork Task from #{user_name} in ##{room_name} at #{time.strftime('%c')}", "description" => results[2], "deadline" => Time.at(results[1].to_f).strftime('%Y-%m-%d %H:%M:%S')}
+            elsif results = /\[info\]\[title\]\[dtext:task_added\]\[\/title\]\[task\said=(\d+).*?\slt=(\d+)\](.*)?\[\/task\]/m.match(text.gsub(/\n/,''))
+              tasks << {"name" => "Chatwork Task[#{results[1]}] from #{user_name} in ##{room_name} at #{time.strftime('%c')}", "description" => results[3], "deadline" => Time.at(results[2].to_f).strftime('%Y-%m-%d %H:%M:%S')}
               puts "Adding a new task!!!"
             end
           end

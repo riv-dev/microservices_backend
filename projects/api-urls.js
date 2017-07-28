acd66903553b801ce438ac5932ec93525cecff75
Ken Le <ken@ryukyu-i.co.jp>
2017-07-28 16:23:13 +0700

    Update API URI's

--- projects/api-urls.js
+++ projects/api-urls.js
@@ -1,4 +1,11 @@
 module.exports = {
+    development: {
+        users_service: 'https://ryukyu-social-dev.cleverword.com/users_service/api',
+        user_photos_service: 'https://ryukyu-social-dev.cleverword.com/user_photos_service/api',
+        projects_service: 'https://ryukyu-social-dev.cleverword.com/projects_service/api',
+        tasks_service: 'https://ryukyu-social-dev.cleverword.com/tasks_service/api'
+    },
+
     local_development: {
         users_service: 'http://ryukyu-users-service:5000',
         user_photos_service: 'http://ryukyu-user-photos-service:5001',
@@ -6,11 +13,12 @@ module.exports = {
         tasks_service: 'http://ryukyu-tasks-service:5003'
     },
 
+    //Same as development
     remote_development: {
-        users_service: 'https://ryukyu-social.cleverword.com/users_service/api',
-        user_photos_service: 'https://ryukyu-social.cleverword.com/user_photos_service/api',
-        projects_service: 'https://ryukyu-social.cleverword.com/projects_service/api',
-        tasks_service: 'https://ryukyu-social.cleverword.com/tasks_service/api'
+        users_service: 'https://ryukyu-social-dev.cleverword.com/users_service/api',
+        user_photos_service: 'https://ryukyu-social-dev.cleverword.com/user_photos_service/api',
+        projects_service: 'https://ryukyu-social-dev.cleverword.com/projects_service/api',
+        tasks_service: 'https://ryukyu-social-dev.cleverword.com/tasks_service/api'
     },
 
     production: {

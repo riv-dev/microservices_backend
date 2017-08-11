5c03fc3763be8b9f251c184f867e2b6e0a22644f
Ken Le <ken@ryukyu-i.co.jp>
2017-08-11 13:10:03 +0700

    Hotfix remove credentials header

--- tasks/app.js
+++ tasks/app.js
@@ -52,7 +52,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
